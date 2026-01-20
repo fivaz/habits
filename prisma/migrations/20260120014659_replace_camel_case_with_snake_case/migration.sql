f5efa60a2b91b34b79a1ef80b62a7240decdefbf
fivaz <31249205+fivaz@users.noreply.github.com>
2026-01-20 02:50:22 +0100

    chore: update field names

---
+++ prisma/migrations/20260120014659_replace_camel_case_with_snake_case/migration.sql
@@ -0,0 +1,137 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `accessToken` on the `account` table. All the data in the column will be lost.
+  - You are about to drop the column `accessTokenExpiresAt` on the `account` table. All the data in the column will be lost.
+  - You are about to drop the column `accountId` on the `account` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `account` table. All the data in the column will be lost.
+  - You are about to drop the column `idToken` on the `account` table. All the data in the column will be lost.
+  - You are about to drop the column `providerId` on the `account` table. All the data in the column will be lost.
+  - You are about to drop the column `refreshToken` on the `account` table. All the data in the column will be lost.
+  - You are about to drop the column `refreshTokenExpiresAt` on the `account` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `account` table. All the data in the column will be lost.
+  - You are about to drop the column `userId` on the `account` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `habit_recipe` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `habit_recipe` table. All the data in the column will be lost.
+  - You are about to drop the column `userId` on the `habit_recipe` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `session` table. All the data in the column will be lost.
+  - You are about to drop the column `expiresAt` on the `session` table. All the data in the column will be lost.
+  - You are about to drop the column `ipAddress` on the `session` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `session` table. All the data in the column will be lost.
+  - You are about to drop the column `userAgent` on the `session` table. All the data in the column will be lost.
+  - You are about to drop the column `userId` on the `session` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `user` table. All the data in the column will be lost.
+  - You are about to drop the column `emailVerified` on the `user` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `user` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `verification` table. All the data in the column will be lost.
+  - You are about to drop the column `expiresAt` on the `verification` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `verification` table. All the data in the column will be lost.
+  - Added the required column `account_id` to the `account` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `provider_id` to the `account` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updated_at` to the `account` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `user_id` to the `account` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updated_at` to the `habit_recipe` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `user_id` to the `habit_recipe` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `expires_at` to the `session` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updated_at` to the `session` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `user_id` to the `session` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updated_at` to the `user` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `expires_at` to the `verification` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updated_at` to the `verification` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "account" DROP CONSTRAINT "account_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "habit_recipe" DROP CONSTRAINT "habit_recipe_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "session" DROP CONSTRAINT "session_userId_fkey";
+
+-- DropIndex
+DROP INDEX "account_userId_idx";
+
+-- DropIndex
+DROP INDEX "habit_recipe_userId_idx";
+
+-- DropIndex
+DROP INDEX "session_userId_idx";
+
+-- AlterTable
+ALTER TABLE "account" DROP COLUMN "accessToken",
+DROP COLUMN "accessTokenExpiresAt",
+DROP COLUMN "accountId",
+DROP COLUMN "createdAt",
+DROP COLUMN "idToken",
+DROP COLUMN "providerId",
+DROP COLUMN "refreshToken",
+DROP COLUMN "refreshTokenExpiresAt",
+DROP COLUMN "updatedAt",
+DROP COLUMN "userId",
+ADD COLUMN     "access_token" TEXT,
+ADD COLUMN     "access_token_expires_at" TIMESTAMP(3),
+ADD COLUMN     "account_id" TEXT NOT NULL,
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "id_token" TEXT,
+ADD COLUMN     "provider_id" TEXT NOT NULL,
+ADD COLUMN     "refresh_token" TEXT,
+ADD COLUMN     "refresh_token_expires_at" TIMESTAMP(3),
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL,
+ADD COLUMN     "user_id" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "habit_recipe" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt",
+DROP COLUMN "userId",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL,
+ADD COLUMN     "user_id" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "session" DROP COLUMN "createdAt",
+DROP COLUMN "expiresAt",
+DROP COLUMN "ipAddress",
+DROP COLUMN "updatedAt",
+DROP COLUMN "userAgent",
+DROP COLUMN "userId",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "expires_at" TIMESTAMP(3) NOT NULL,
+ADD COLUMN     "ip_address" TEXT,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL,
+ADD COLUMN     "user_agent" TEXT,
+ADD COLUMN     "user_id" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "user" DROP COLUMN "createdAt",
+DROP COLUMN "emailVerified",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "email_verified" BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "verification" DROP COLUMN "createdAt",
+DROP COLUMN "expiresAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "expires_at" TIMESTAMP(3) NOT NULL,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
+
+-- CreateIndex
+CREATE INDEX "account_user_id_idx" ON "account"("user_id");
+
+-- CreateIndex
+CREATE INDEX "habit_recipe_user_id_idx" ON "habit_recipe"("user_id");
+
+-- CreateIndex
+CREATE INDEX "session_user_id_idx" ON "session"("user_id");
+
+-- AddForeignKey
+ALTER TABLE "habit_recipe" ADD CONSTRAINT "habit_recipe_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "session" ADD CONSTRAINT "session_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "account" ADD CONSTRAINT "account_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
