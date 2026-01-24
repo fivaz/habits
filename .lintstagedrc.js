b1bf2c33d64292b9548b6e4bb382f33836dd5801
fivaz <31249205+fivaz@users.noreply.github.com>
2026-01-24 04:21:35 +0100

    chore: improve lintstage speed

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,3 +1,5 @@
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
-	"*.{js,ts,jsx,tsx,json,css,scss,md}": ["pnpm run format"],
+	"*.{js,ts,jsx,tsx}": ["pnpm exec eslint --fix", "pnpm exec prettier --write"],
+	"*.{json,css,scss,md}": ["pnpm exec prettier --write"],
 };
